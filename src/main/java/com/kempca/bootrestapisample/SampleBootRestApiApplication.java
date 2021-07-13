2461c8834b8f39b41eaa175fbbab7aed95d0234d
kempca <kemp108@gmail.com>
2021-07-13 12:26:18 -0600

    Initial commit from Spring Initializer

---
+++ src/main/java/com/kempca/bootrestapisample/SampleBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.kempca.bootrestapisample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBootRestApiApplication.class, args);
+	}
+
+}
